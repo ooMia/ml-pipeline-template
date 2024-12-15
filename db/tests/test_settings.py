796a1df6c30054063d57e28b1dd415ea4c86f1ad
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2024-12-16 08:37:36 +0900

    🐛 Fix : ModuleNotFoundError

--- db/tests/test_settings.py
+++ db/tests/test_settings.py
@@ -1,12 +1,12 @@
 def test_dir_exist():
     import os
-    from db.tests import MYSQL_DATA_DIR
+    from db import MYSQL_DATA_DIR
     assert os.path.exists(MYSQL_DATA_DIR)
 
 
 def test_initial_connection():
     import mysql.connector
-    from db.tests import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT, MYSQL_HOST
+    from db import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_PORT, MYSQL_HOST
     with mysql.connector.connect(
             host=MYSQL_HOST,
             port=MYSQL_PORT,
